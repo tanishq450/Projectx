037841ad8fa4211691e4848757085e7b1dbca74e
Tanishq <140788303+tanishq450@users.noreply.github.com>
2026-03-07 00:24:59 +0530

    commit

--- ProjecX/Llama_index/chroma_client.py
+++ ProjecX/Llama_index/chroma_client.py
@@ -1,12 +1,20 @@
 import chromadb
+import os
 
 _clients = {}
 
 def get_chroma_client(persist_dir: str) -> chromadb.Client:
     if persist_dir not in _clients:
+
+        # create directory if it does not exist
+        if not os.path.exists(persist_dir):
+            os.makedirs(persist_dir)
+
         _clients[persist_dir] = chromadb.Client(
-            settings=chromadb.Settings(
-                persist_directory=persist_dir
+            chromadb.Settings(
+                persist_directory=persist_dir,
+                anonymized_telemetry=False
             )
         )
+
     return _clients[persist_dir]
